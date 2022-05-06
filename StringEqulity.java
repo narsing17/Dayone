fbef6ebd6bc70c0bfcc81d350ae230030ea616fc
narsing17 <narsingp14@gmail.com>
2022-05-07 00:51:10 +0530

    checking Two Strings

---
+++ StringEqulity.java
@@ -0,0 +1,21 @@
+package MyClass;
+
+import java.util.Scanner;
+
+public class StringEqulity {
+    public static void main(String args[]) {
+        String str1, str2;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first String");
+        str1 = scanner.nextLine();
+
+        System.out.println("Enter second String");
+        str2 = scanner.nextLine();
+
+        if (str1.equals(str2))
+            System.out.print("Equal Strings");
+        else
+            System.out.print("UnEqual Strings");
+    }
+}

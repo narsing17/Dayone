0f41c2ca1751af17dccac62517e3573c8f6e3c99
narsing17 <narsingp14@gmail.com>
2022-05-07 00:44:26 +0530

    Displaying primitive data types

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,24 @@
+package MyClass;
+
+public class PrimitiveDataTypes {
+    static boolean val1;
+    static float val2;
+    static int val3;
+    static double val4;
+    static String val5;
+    static long val6;
+    static char val7;
+    static byte val8;
+
+    public static void main(String[] args) {
+        System.out.println("Defult values of all data types are ");
+        System.out.println("boolean=" + val1);
+        System.out.println("float=" + val2);
+        System.out.println("int=" + val3);
+        System.out.println("double=" + val4);
+        System.out.println("String=" + val5);
+        System.out.println("long=" + val6);
+        System.out.println("char=" + val7);
+        System.out.println("byte=" + val8);
+    }
+}
